ff55fef92e8ea8f87004a70dd198495d405e2787
tmdk <tobiasvandijk@gmail.com>
2017-05-09 13:42:38 +0200

    Add command for non-exiting ssh envs

--- 1l/add-to-know-hosts.sh
+++ 1l/add-to-know-hosts.sh
@@ -1,1 +1,5 @@
+# for automatically exiting ssh environments (bitbucket, ssh)
 ssh -o StrictHostKeyChecking=no -T git@bitbucket.org
+
+# for normal ssh envs
+ssh -o StrictHostKeyChecking=no -T user@example.org exit
